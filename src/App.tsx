7f77517ff6459959212b623feb930eb2ad6f79c5
Jakub Zarębski <jakubzarebski0@gmail.com>
2025-01-04 21:24:20 +0100

    init add tailwind and daisyui

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import TododsTable from "./TodosTable"
+
+function App() {
+
+
+  return (
+  <div className=" w-screen h-screen p-10 flex justify-center items-center">
+      <div className="container columns-1 md:columns-2">
+<TododsTable/>
+        <div className="">
+          test2
+        </div>
+      </div>
+
+  </div>
+  )
+}
+
+export default App
